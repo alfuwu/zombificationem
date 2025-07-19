b0f9b69c38c79abede14da9abb225a44cb71252c
alfuwu <eldritch.alfred@outlook.com>
2025-07-19 13:49:23 -0700

    minor fixes

--- src/client/java/com/alfred/zombification/mixin/client/BipedEntityModelMixin.java
+++ src/client/java/com/alfred/zombification/mixin/client/BipedEntityModelMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.CrossbowPosing;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.HostileEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
